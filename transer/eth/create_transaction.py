b6916a005181504d275013a57e5354dab47d9cb3
Oleg Gaidukov <ogaidukov@gmail.com>
2017-12-19 00:07:03 +0300

    fix all the Ethereum bugs and indeterminate behavior

--- transer/eth/create_transaction.py
+++ transer/eth/create_transaction.py
@@ -1,8 +1,11 @@
+import decimal
+import codecs
+
 import rlp
 from ethereum import transactions
 from web3 import Web3
 
-from transer.eth.validate_address import validate_addr_format, validate_existence_addr_
+from transer.eth.validate_address import normalize_addr, validate_existence_addr_
 from transer.exceptions import EthAddressIntegrityException
 
 from transer.eth import eth_divider, _eth_dispatcher
@@ -34,41 +37,41 @@ def create_transaction(web3_url, src_addr, dst_addr, amount, gas_price):
     :param web3_url: web3 RPC url as str
     :param src_addr: source address as hex str
     :param dst_addr: destination address as hex str
-    :param amount: amount of funds to transfer, in Ether (!)
-    :param gas_price: price of gas, in Ether (!)
+    :param amount: amount of funds to transfer, in Ether (!) as str or float (may lead to imprecision)
+    :param gas_price: price of gas, in Ether (!) as str or float (may lead to imprecision)
     :return: unsigned transaction as hex str
     """
+
     provider = Web3.HTTPProvider(web3_url)
     web3_inst = Web3(provider)
 
-    amount *= eth_divider   # convert to Wei
-    gas_price *= gas_price  # convert to Wei
+    amount = decimal.Decimal(amount) * eth_divider          # convert to Wei
+    gas_price = decimal.Decimal(gas_price) * eth_divider    # convert to Wei
     fee = gas_price * TRANSACTION_GAS
 
-    if not validate_addr_format(src_addr):
-        raise EthAddressIntegrityException(f'Invalid source address {src_addr}')
+    src_addr = normalize_addr(src_addr)
 
     # берём nonce тут, чтобы использовать его как optimistic lock для создаваемой транзакции
     nonce = web3_inst.eth.getTransactionCount(src_addr)
 
-    if not validate_existence_addr_(
+    if validate_existence_addr_(
             web3_inst=web3_inst,
             addr=src_addr,
-            estimate_spendable=amount + fee):
+            estimate_spendable=(amount + fee) / eth_divider  # convert back to Eth
+    ) is not True:
         raise EthAddressIntegrityException(f'No enough Ether on source address {src_addr} '
                                            f'to perform the transaction')
 
-    if not validate_addr_format(dst_addr):
-        raise EthAddressIntegrityException(f'Invalid destination address {dst_addr}')
+    dst_addr = normalize_addr(dst_addr)
 
     tx = transactions.Transaction(
         nonce=nonce,
-        gasprice=gas_price,
+        gasprice=int(gas_price.to_integral_exact()),
         startgas=TRANSACTION_GAS,
         to=dst_addr,
-        value=amount,
+        value=int(amount.to_integral_exact()),
         data=''
     )
 
-    unsigned_tx_h = rlp.encode(tx).encode('hex')
+    unsigned_tx_h = codecs.encode(rlp.encode(tx), 'hex')
     return unsigned_tx_h
