2082cb39ed457f15f7f94afa82002d24d6619da8
Oleg Gaidukov <ogaidukov@gmail.com>
2018-01-30 00:10:28 +0300

    broader listening address in staging runner.py

--- devel/runner.py
+++ devel/runner.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
     daemon.run(
         db_uri='postgresql://ogaidukov@127.0.0.1:5432/btc_test',
-        listen_host='127.0.0.1',
+        listen_host='0.0.0.0',
         listen_port=8000,
         workers=10,
         btc_masterkey_name='btc_main',
