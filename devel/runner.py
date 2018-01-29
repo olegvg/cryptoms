17221935fe472960e3aa58962d53a593ec73d572
Oleg Gaidukov <ogaidukov@gmail.com>
2018-01-29 23:50:16 +0300

    runner.py for staging env

---
+++ devel/runner.py
@@ -0,0 +1,23 @@
+import os.path
+import sys
+
+
+if __name__ == '__main__':
+    selfpath = os.path.dirname(os.path.abspath(__file__))
+    rootpath = os.path.join(selfpath, '..')
+    sys.path.append(rootpath)
+
+    from transer import daemon
+
+    daemon.run(
+        db_uri='postgresql://ogaidukov@127.0.0.1:5432/btc_test',
+        listen_host='127.0.0.1',
+        listen_port=8000,
+        workers=10,
+        btc_masterkey_name='btc_main',
+        eth_masterkey_name='eth_main',
+        btcd_instance_name='main_instance',
+        etcd_instance_uri='',
+        deposit_notification_endpoint='https://staging.payments.cryptology.com/api/internal/cryptopay/deposit'
+    )
+
