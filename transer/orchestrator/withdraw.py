c9a8db9801b357e9737409657d68bb926e61b706
Oleg Gaidukov <ogaidukov@gmail.com>
2018-02-05 04:01:02 +0300

    lots of bugs smashed in btc withdrawal mech

--- transer/orchestrator/withdraw.py
+++ transer/orchestrator/withdraw.py
@@ -6,7 +6,7 @@ from transer import config
 from transer.exceptions import TransactionInconsistencyError
 from transer.db import eth, btc, transaction, sqla_session
 from transer.types import CryptoCurrency, WithdrawalStatus
-from transer.btc.create_transaction import transaction_fee_per_byte, create_transaction
+from transer.btc.create_transaction import calculate_transaction_fee, create_transaction
 from transer.btc.sign_transaction import sign_transaction
 from transer.btc.send_transaction import send_transaction
 from transer.btc.monitor_transaction import get_txid_status
@@ -21,7 +21,6 @@ def withdraw_btc(u_txid, address, amount):
     :param amount: объём битокойнов
     :return:
     """
-
     # explicitly prohibit withdrawals to addresses belong to Cryptology
     # i.e. cryptopayments between clients via external Bitcoin network
     check_addr = btc.Address.query.filter(
@@ -30,7 +29,24 @@ def withdraw_btc(u_txid, address, amount):
     if check_addr > 0:  # == 1 actually
         return WithdrawalStatus.FAILED.value
 
-    def brand_new_transacton():
+    u_txid_q = transaction.CryptoWithdrawTransaction.query.filter(
+        transaction.CryptoWithdrawTransaction.u_txid == u_txid
+    )
+
+    try:
+        crypto_transaction = u_txid_q.one()
+        return crypto_transaction.status
+    except MultipleResultsFound as e:
+        raise TransactionInconsistencyError(f'Multiple transaction records found for {u_txid}. Report the bug') from e
+    except NoResultFound:
+        crypto_transaction = transaction.CryptoWithdrawTransaction(
+            u_txid=u_txid,
+            currency=CryptoCurrency.BITCOIN.value,
+            status=WithdrawalStatus.FAILED.value
+
+        )
+        sqla_session.add(crypto_transaction)
+
         btcd_instance_name = config['btcd_instance_name']
         key_name = config['btc_masterkey_name']
         masterkey_q = btc.MasterKey.query.filter(
@@ -43,7 +59,7 @@ def withdraw_btc(u_txid, address, amount):
         )
         bitcoind_inst = bitcoind_instance_q.one()
 
-        # given that num of UTXOs ~ num of Adresses and fee is ~$2 per UTXO
+        # given that num of UTXOs ~ num of Addresses and fee is ~$2 per UTXO
         # so let it be no more 10 UTXOs/Addresses to feed the destination at cost ~$20 and one wallet for change
         ordered_adresses_q = btc.Address.query.filter(
             btc.Address.bitcoind_inst == bitcoind_inst,
@@ -51,19 +67,24 @@ def withdraw_btc(u_txid, address, amount):
             btc.Address.address != address
         ).order_by(desc(btc.Address.amount)).limit(11)
         candidates = ordered_adresses_q.all()
+
         change_address = candidates.pop(-1)
 
         estimate_amount = decimal.Decimal(0.0)
-        fee_per_byte = transaction_fee_per_byte(bitcoind_inst, preferred_blocks=5)
 
-        # bytes = 148 * in + 34 * out + 10 ± in
-        projected_fee = fee_per_byte * (148 * len(candidates) + 34 * 2 + 10 + len(candidates))
+        projected_fee = calculate_transaction_fee(
+            bt_name=btcd_instance_name,
+            sources=[c.address for c in candidates], destination=address,
+            change=change_address.address,
+            preferred_blocks=5  # Anton don't like such numbers :-)
+        )
+        print(projected_fee)
 
         src_address_objs = []
         for c in candidates:
             estimate_amount += c.amount
             src_address_objs.append(c)
-            if estimate_amount >= amount + projected_fee:
+            if estimate_amount > amount + projected_fee:
                 break
         if estimate_amount < amount + projected_fee:
             crypto_transaction.status = WithdrawalStatus.FAILED.value
@@ -72,16 +93,25 @@ def withdraw_btc(u_txid, address, amount):
 
         src_addresses = [x.address for x in src_address_objs]
 
-        _, trx = create_transaction(
+        # Equation 'projected_fee >= actual_fee' is always true
+        actual_fee = calculate_transaction_fee(
+            bt_name=btcd_instance_name,
+            sources=[src_addresses],
+            destination=address,
+            change=change_address.address,
+            preferred_blocks=5  # Anton don't like such numbers :-)
+        )
+
+        trx, _ = create_transaction(
             bt_name=bitcoind_inst.instance_name,
             sources=src_addresses,
             destination=address,
             amount=amount,
-            change=change_address,
-            fee=projected_fee
+            change=change_address.address,
+            fee=actual_fee
         )
 
-        _, signed_trx = sign_transaction(
+        signed_trx, _ = sign_transaction(
             bt_name=bitcoind_inst.instance_name,
             signing_addrs=src_addresses,
             trx=trx
@@ -99,45 +129,25 @@ def withdraw_btc(u_txid, address, amount):
         crypto_transaction.status = WithdrawalStatus.PENDING.value
         crypto_transaction.txids = [txid]
 
-        change_address_log = btc.ChangeTransactionLog(
-            # change_address=change_address,
-            change_tx_id=txid
-        )
-        sqla_session.add(change_address_log)
+        # change_address_log = btc.ChangeTransactionLog(
+        #     # change_address=change_address,
+        #     change_tx_id=txid
+        # )
+        # sqla_session.add(change_address_log)
 
         # all the funds move to change address
-        for a in src_addresses:
+        for a in src_address_objs:
             a.amount = decimal.Decimal(0.0)
 
         sqla_session.commit()
         return crypto_transaction.status
 
-    u_txid_q = transaction.CryptoWithdrawTransaction.query.filter(
-        transaction.CryptoWithdrawTransaction.u_txid == u_txid
-    )
 
-    try:
-        crypto_transaction = u_txid_q.one()
-    except MultipleResultsFound as e:
-        raise TransactionInconsistencyError(f'Multiple transaction records found for {u_txid}. Report the bug') from e
-    except NoResultFound:
-        crypto_transaction = transaction.CryptoWithdrawTransaction(
-            u_txid=u_txid,
-            currency=CryptoCurrency.BITCOIN.value,
-            status=WithdrawalStatus.PENDING.value
-
-        )
-        sqla_session.add(crypto_transaction)
-        sqla_session.commit()
-        return brand_new_transacton()
-
-    return crypto_transaction.status
-
-
-def withdrawal_status_btc(txids):
+def withdrawal_status_btc(crypto_transaction):
     btcd_instance_name = config['btcd_instance_name']
+
     try:
-        txid = txids[0]  # in btc, only one txid per transaction
+        txid = crypto_transaction.txids[0]  # in btc, only one txid per transaction
     except (KeyError, TypeError):
         return WithdrawalStatus.FAILED.value
 
@@ -148,13 +158,12 @@ def withdrawal_status_btc(txids):
 
     confirmations = tx_info['confirmations']
 
-    status = WithdrawalStatus.FAILED.value
-    if confirmations >= 6:
-        status = WithdrawalStatus.COMPLETED.value
-    elif confirmations >= 0:
-        status = WithdrawalStatus.PENDING.value
-
-    return status
+    if confirmations >= 6 and crypto_transaction.status != WithdrawalStatus.COMPLETED.value:
+        crypto_transaction.status = WithdrawalStatus.COMPLETED.value
+        crypto_transaction.is_acknowledged = False
+    elif confirmations >= 0 and crypto_transaction.status != WithdrawalStatus.PENDING.value:
+        crypto_transaction.status = WithdrawalStatus.PENDING.value
+        crypto_transaction.is_acknowledged = False
 
 
 def withdraw_eth(u_txid, address, amount):
