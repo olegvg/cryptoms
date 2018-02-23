ab0c11272679053d23874ddb1600153bd7c40731
Oleg Gaidukov <ogaidukov@gmail.com>
2018-02-23 23:32:05 +0300

    fucking debugging

--- transer/utils.py
+++ transer/utils.py
@@ -154,7 +154,7 @@ def init_logging():
     }
 
     import sys, pprint
-    sys.stderr.write(pprint.pformat(logging_config))
+    raise Exception(pprint.pformat(logging_config))
 
     logging.config.dictConfig(logging_config)
     logging.getLogger('initial').exception('Logger init finished')
